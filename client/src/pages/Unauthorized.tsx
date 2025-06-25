f933ff212ce4c284be235715211c750ae52c76b6
Alish Twanabasu <45710495+AlishTwanabasu@users.noreply.github.com>
2025-06-24 20:35:17 -0400

    HEL-129: Update Unauthorized.tsx page UI

--- client/src/pages/Unauthorized.tsx
+++ client/src/pages/Unauthorized.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import DangerousIcon from "@mui/icons-material/Dangerous";
 
 const Unauthorized = () => {
   const navigate = useNavigate();
@@ -16,12 +17,22 @@ const Unauthorized = () => {
         p: 4,
       }}
     >
-      <Typography variant="h3" gutterBottom>
-        🚫 Unauthorized
-      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 1,
+          mb: 1,
+        }}
+      >
+        <DangerousIcon fontSize="large" color="error" />
+        <Typography variant="h3">Unauthorized</Typography>
+      </Box>
+
       <Typography variant="body1" sx={{ mb: 3 }}>
         You do not have permission to access this page.
       </Typography>
+
       <Button variant="contained" color="primary" onClick={() => navigate(-1)}>
         Go Back
       </Button>
