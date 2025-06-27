0bbc429600a6897e29f5d6d7355ef7f1dd88da50
Aakarshan Thapa <akarsan.thapa@gmail.com>
2025-06-27 17:25:59 -0400

    HEL-163: Add protected to category and status GET routes

--- server/src/routes/category-routes.ts
+++ server/src/routes/category-routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllCategories } from "../controllers/category-controllers";
+import { protect } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/", getAllCategories); // GET /api/v1/categories
+router.get("/", protect, getAllCategories); // GET /api/v1/categories
 
 export default router;
--- server/src/routes/status-routes.ts
+++ server/src/routes/status-routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllStatuses } from "../controllers/status-controllers";
+import { protect } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/", getAllStatuses); // GET /api/v1/statuses
+router.get("/", protect, getAllStatuses); // GET /api/v1/statuses
 
 export default router;
