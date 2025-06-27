609f32e507751cfd512918d0b9a961766ed80b4a
Aakarshan Thapa <akarsan.thapa@gmail.com>
2025-06-27 16:59:29 -0400

    Update status model to include description

--- server/src/models/status-model.ts
+++ server/src/models/status-model.ts
@@ -1,11 +1,14 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+// Interface for Status document
 export interface IStatus extends Document {
   name: string;
+  description?: string;
 }
 
 const statusSchema = new Schema<IStatus>({
   name: { type: String, required: true, unique: true },
+  description: { type: String },
 });
 
 export default mongoose.model<IStatus>("Status", statusSchema);
