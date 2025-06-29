f0e50ac6c2a33609250f5032f47a4df796c2ede8
Aakarshan Thapa <akarsan.thapa@gmail.com>
2025-06-29 16:41:34 -0400

    Add ticket routes to main router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,10 +4,12 @@ import userRoutes from "./user-routes";
 import attachmentRoutes from "./attachment-routes";
 import categoryRoutes from "./category-routes";
 import statusRoutes from "./status-routes";
+import ticketRoutes from "./ticket-routes";
 
 router.use("/users", userRoutes);
 router.use("/attachments", attachmentRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/statuses", statusRoutes);
+router.use("/tickets", ticketRoutes);
 
 export default router;
