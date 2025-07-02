29c14384bd8ee1d4cd75d385583be2ec1233979b
vishalbhaika <c0930167@mylambton.ca>
2025-07-02 16:04:13 -0400

    HEL-141: add route to fetch a ticket by ID for the logged-in client

--- server/src/routes/ticket-routes.ts
+++ server/src/routes/ticket-routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { createTicket, myTickets } from "../controllers/ticket-controllers";
+import {
+  createTicket,
+  myTickets,
+  getTicketById,
+} from "../controllers/ticket-controllers";
 import { protect } from "../middlewares/auth";
 import { authorizeRoles } from "../middlewares/authorize";
 
@@ -7,5 +11,6 @@ const router = express.Router();
 
 router.post("/", protect, authorizeRoles("client"), createTicket);
 router.get("/my", protect, authorizeRoles("client"), myTickets);
+router.get("/:ticketId", protect, authorizeRoles("client"), getTicketById);
 
 export default router;
